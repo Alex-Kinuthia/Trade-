95c29b0059e5c96fc735be62a555fc3aaea7788c
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-26 19:54:07 +0300

    modified files

--- app/src/main/java/com/example/alex/trade/TradeActivity.java
+++ app/src/main/java/com/example/alex/trade/TradeActivity.java
@@ -3,14 +3,17 @@ package com.example.alex.trade;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TradeActivity extends AppCompatActivity {
+    private TextView mProductTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade);
         Intent intent = getIntent();
-        String location = intent.getStringExtra("product");
+        String product = intent.getStringExtra("product");
+        mProductTextView.setText("Here are all the products: " + product);
     }
 }
